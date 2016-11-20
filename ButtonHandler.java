de2cfb5d30fee3a058230aa7713b27fd3d166f0c
Mark Parfeniuk <mark.parfeniuk@itcollege.ee>
2016-11-20 23:50:04 +0200

    Functionality of my buttons

---
+++ ButtonHandler.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coffeemachine;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ButtonHandler {
+    
+    public ButtonHandler(){
+        
+    }
+    
+    public void buttonActive(java.awt.event.ActionEvent event){
+        System.out.println("Latte");
+    }
+}

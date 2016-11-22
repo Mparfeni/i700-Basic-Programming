792e4f977bd36a512fb020bb681994d483a8586c
Mark Parfeniuk <mark.parfeniuk@itcollege.ee>
2016-11-22 11:37:37 +0200

    Added new functionality
    All the names will be written to arraylist and then to the file

--- ButtonHandler.java
+++ ButtonHandler.java
@@ -1,42 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package coffeemachine;
 
-/**
- *
- * @author ASUS
- */
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+
 public class ButtonHandler {
+    PopularCoffee pc = new PopularCoffee();
     
     public ButtonHandler(){
-        
+     
     }
     
     public void buttonActive(java.awt.event.ActionEvent event){
         System.out.println("Espresso");
+        PopularCoffee.array.add("Espresso");
+        pc.writeStatistics();
     }
     public void buttonActive1(java.awt.event.ActionEvent event){
         System.out.println("Lungo Entkoff");
+        PopularCoffee.array.add("Lungo Entkoff");
+        pc.writeStatistics();
     }
     public void buttonActive2(java.awt.event.ActionEvent event){
         System.out.println("Cappuccino");
+        PopularCoffee.array.add("Cappuccino");
+        pc.writeStatistics();
     }
     public void buttonActive3(java.awt.event.ActionEvent event){
         System.out.println("Cappuccino Vanilla");
+        PopularCoffee.array.add("Cappuccino Vanilla");
+        pc.writeStatistics();
     }
     public void buttonActive4(java.awt.event.ActionEvent event){
         System.out.println("Lungo");
+        PopularCoffee.array.add("Lungo");
+        pc.writeStatistics();
     }
     public void buttonActive5(java.awt.event.ActionEvent event){
         System.out.println("Latte Macchiato");
+        PopularCoffee.array.add("Latte Macchiato");
+        pc.writeStatistics();
     }
     public void buttonActive6(java.awt.event.ActionEvent event){
         System.out.println("Kakao");
+        PopularCoffee.array.add("Kakao");
+        pc.writeStatistics();
     }
     public void buttonActive7(java.awt.event.ActionEvent event){
         System.out.println("Tee mit Milch");
+        PopularCoffee.array.add("Tee mit Milch");
+        pc.writeStatistics();
     }
+    
 }
+

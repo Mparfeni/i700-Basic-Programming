c2297aa584367e04577e616998b9fdeacc5e50e2
Mark Parfeniuk <mark.parfeniuk@itcollege.ee>
2016-11-23 11:15:00 +0200

    Add sound effects

--- ButtonHandler.java
+++ ButtonHandler.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 public class ButtonHandler {
     PopularCoffee pc = new PopularCoffee();
+    Sound sn = new Sound();
     
     public ButtonHandler(){
      
@@ -17,41 +18,49 @@ public class ButtonHandler {
         System.out.println("Espresso");
         PopularCoffee.array.add("Espresso");
         pc.writeStatistics();
+        sn.playSound();
     }
     public void buttonActive1(java.awt.event.ActionEvent event){
         System.out.println("Lungo Entkoff");
         PopularCoffee.array.add("Lungo Entkoff");
         pc.writeStatistics();
+        sn.playSound();
     }
     public void buttonActive2(java.awt.event.ActionEvent event){
         System.out.println("Cappuccino");
         PopularCoffee.array.add("Cappuccino");
         pc.writeStatistics();
+        sn.playSound();
     }
     public void buttonActive3(java.awt.event.ActionEvent event){
         System.out.println("Cappuccino Vanilla");
         PopularCoffee.array.add("Cappuccino Vanilla");
         pc.writeStatistics();
+        sn.playSound();
     }
     public void buttonActive4(java.awt.event.ActionEvent event){
         System.out.println("Lungo");
         PopularCoffee.array.add("Lungo");
         pc.writeStatistics();
+        sn.playSound();
     }
     public void buttonActive5(java.awt.event.ActionEvent event){
         System.out.println("Latte Macchiato");
         PopularCoffee.array.add("Latte Macchiato");
         pc.writeStatistics();
+        sn.playSound();
     }
     public void buttonActive6(java.awt.event.ActionEvent event){
         System.out.println("Kakao");
         PopularCoffee.array.add("Kakao");
         pc.writeStatistics();
+        sn.playSound();
     }
     public void buttonActive7(java.awt.event.ActionEvent event){
         System.out.println("Tee mit Milch");
         PopularCoffee.array.add("Tee mit Milch");
         pc.writeStatistics();
+        sn.playSound();
     }
     
 }

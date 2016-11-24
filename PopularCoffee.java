a491496973121890760337cf009e3346c28e836f
Mark Parfeniuk <mark.parfeniuk@itcollege.ee>
2016-11-24 23:58:05 +0200

    Added method readFile

--- PopularCoffee.java
+++ PopularCoffee.java
@@ -1,35 +1,51 @@
 package coffeemachine;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.Writer;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PopularCoffee {
     private String fileName;
     private String titles;
-    public static ArrayList array;
-    
+    public static HashMap<String, Integer> coffeeStatistics;
     
     public PopularCoffee(){
         fileName = "CoffeeNames.txt";
         titles = " " ;
-        array = new ArrayList();
+        coffeeStatistics = new HashMap<>();
     }
-  
     
     public void writeStatistics(){
          try {
+            
             FileWriter fw = new FileWriter(fileName);
             Writer output = new BufferedWriter(fw);
-            int size = array.size();
+            int size = coffeeStatistics.size();
             for(int i=0;i<size;i++) {
-                output.write(array.get(i).toString() + "\n");
+                output.write(" " + coffeeStatistics.get(i).toString() + "\n");
             }
             output.close();
         } catch (Exception e) {
             System.out.println("I cannot create that file");
         }
     }
-    
+     public void readFile() {
+       try {
+        BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] temp = line.split(",");
+            String Key = temp[0];
+            String Value = temp[1];
+            coffeeStatistics.put(Key, Integer.parseInt(Value));
+            reader.close();
+        }}catch(Exception e){
+            System.out.println("I cannor read that file");
+         }
+       
+     }
 }

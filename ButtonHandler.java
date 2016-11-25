6e27309a016a6479dcd8fdbc0733eea3cc5c7c51
Mark Parfeniuk <mark.parfeniuk@itcollege.ee>
2016-11-26 01:55:52 +0200

    Small optimization
    Added small changes and sound for all buttons

--- ButtonHandler.java
+++ ButtonHandler.java
@@ -1,66 +1,59 @@
-
 package coffeemachine;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.Writer;
-import java.util.ArrayList;
 
 public class ButtonHandler {
     PopularCoffee pc = new PopularCoffee();
     Sound sn = new Sound();
     
     public ButtonHandler(){
-     
+        
     }
     
     public void buttonActive(java.awt.event.ActionEvent event){
         System.out.println("Espresso");
-        PopularCoffee.array.add("Espresso");
-        pc.writeStatistics();
+        pc.writeFile("Espresso");
         sn.playSound();
+       
     }
     public void buttonActive1(java.awt.event.ActionEvent event){
         System.out.println("Lungo Entkoff");
-        PopularCoffee.array.add("Lungo Entkoff");
-        pc.writeStatistics();
+        pc.writeFile("Lungo Entkoff");
         sn.playSound();
+ 
     }
     public void buttonActive2(java.awt.event.ActionEvent event){
         System.out.println("Cappuccino");
-        PopularCoffee.array.add("Cappuccino");
-        pc.writeStatistics();
+        pc.writeFile("Cappuccino");
         sn.playSound();
+
     }
     public void buttonActive3(java.awt.event.ActionEvent event){
         System.out.println("Cappuccino Vanilla");
-        PopularCoffee.array.add("Cappuccino Vanilla");
-        pc.writeStatistics();
+        pc.writeFile("Cappuccino Vanilla");
         sn.playSound();
+
     }
     public void buttonActive4(java.awt.event.ActionEvent event){
         System.out.println("Lungo");
-        PopularCoffee.array.add("Lungo");
-        pc.writeStatistics();
+        pc.writeFile("Lungo");
         sn.playSound();
+
     }
     public void buttonActive5(java.awt.event.ActionEvent event){
         System.out.println("Latte Macchiato");
-        PopularCoffee.array.add("Latte Macchiato");
-        pc.writeStatistics();
+        pc.writeFile("Latte Macchiato");
         sn.playSound();
+
     }
     public void buttonActive6(java.awt.event.ActionEvent event){
         System.out.println("Kakao");
-        PopularCoffee.array.add("Kakao");
-        pc.writeStatistics();
+        pc.writeFile("Kakao");
         sn.playSound();
     }
     public void buttonActive7(java.awt.event.ActionEvent event){
         System.out.println("Tee mit Milch");
-        PopularCoffee.array.add("Tee mit Milch");
-        pc.writeStatistics();
+        pc.writeFile("Tee mit Milch");
         sn.playSound();
+        
     }
     
 }

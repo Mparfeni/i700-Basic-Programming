f7e878085eb31c3f7cea8f621f4c5ea22f6ca807
Mark Parfeniuk <mark.parfeniuk@itcollege.ee>
2016-11-27 23:02:48 +0200

    Found  new way how to play sound

--- ButtonHandler.java
+++ ButtonHandler.java
@@ -1,60 +1,68 @@
 package coffeemachine;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.URL;
+
 public class ButtonHandler {
     PopularCoffee pc = new PopularCoffee();
-    Sound sn = new Sound();
     
     public ButtonHandler(){
         
+    }
+    public void play(String nomeDoAudio){
+        URL url = ButtonHandler.class.getResource(nomeDoAudio+".wav");
+        AudioClip audio = Applet.newAudioClip(url);
+        audio.play();
     }
     
     public void buttonActive(java.awt.event.ActionEvent event){
         System.out.println("Espresso");
         pc.writeFile("Espresso");
-        sn.playSound();
+        play("Coffee");
+        
+        
        
     }
     public void buttonActive1(java.awt.event.ActionEvent event){
         System.out.println("Lungo Entkoff");
         pc.writeFile("Lungo Entkoff");
-        sn.playSound();
+        play("Coffee");
  
     }
     public void buttonActive2(java.awt.event.ActionEvent event){
         System.out.println("Cappuccino");
         pc.writeFile("Cappuccino");
-        sn.playSound();
+        play("Coffee");
 
     }
     public void buttonActive3(java.awt.event.ActionEvent event){
         System.out.println("Cappuccino Vanilla");
         pc.writeFile("Cappuccino Vanilla");
-        sn.playSound();
+        play("Coffee");
 
     }
     public void buttonActive4(java.awt.event.ActionEvent event){
         System.out.println("Lungo");
         pc.writeFile("Lungo");
-        sn.playSound();
+        play("Coffee");
 
     }
     public void buttonActive5(java.awt.event.ActionEvent event){
         System.out.println("Latte Macchiato");
         pc.writeFile("Latte Macchiato");
-        sn.playSound();
+        play("Coffee");
 
     }
     public void buttonActive6(java.awt.event.ActionEvent event){
         System.out.println("Kakao");
         pc.writeFile("Kakao");
-        sn.playSound();
+        play("Coffee");
     }
     public void buttonActive7(java.awt.event.ActionEvent event){
         System.out.println("Tee mit Milch");
         pc.writeFile("Tee mit Milch");
-        sn.playSound();
-        
-    }
-    
+        play("Coffee");   
+    }  
 }
 

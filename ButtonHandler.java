b208bedf751ef10a74cc20ced9263ceb10270435
Mark Parfeniuk <mark.parfeniuk@itcollege.ee>
2016-12-05 05:31:46 +0200

    Made my code smaller, some small changes
    Added javadoc

--- ButtonHandler.java
+++ ButtonHandler.java
@@ -1,68 +1,36 @@
-package coffeemachine;
+package Main;
 
+import statistics.PopularCoffee;
 import java.applet.Applet;
 import java.applet.AudioClip;
 import java.net.URL;
 
-public class ButtonHandler {
-    PopularCoffee pc = new PopularCoffee();
-    
-    public ButtonHandler(){
-        
+/**Class is made for handling button pressing and
+ * playing music
+ * @author ASUS
+ */
+public class ButtonHandler { 
+    private PopularCoffee pc;
+    public ButtonHandler(PopularCoffee popularCoffee){
+        pc = popularCoffee;
     }
-    public void play(String nomeDoAudio){
-        URL url = ButtonHandler.class.getResource(nomeDoAudio+".wav");
+
+    /**This method will play music*/
+    public void playMusic(String Audio){
+        URL url = ButtonHandler.class.getResource("/Coffee.wav");
         AudioClip audio = Applet.newAudioClip(url);
         audio.play();
     }
     
-    public void buttonActive(java.awt.event.ActionEvent event){
-        System.out.println("Espresso");
-        pc.writeFile("Espresso");
-        play("Coffee");
-        
-        
-       
+    /**This method will do some act(show our coffee name, write info to statistics,
+     * play music) if we call it.
+     * @param event - act, that will happen
+     * @param coffeeName - name of coffee, that we want
+     */
+    public void buttonPressed(java.awt.event.ActionEvent event, String coffeeName){
+        System.out.println(coffeeName);
+        pc.writeToStatistics(coffeeName);
+        playMusic("Coffee");
     }
-    public void buttonActive1(java.awt.event.ActionEvent event){
-        System.out.println("Lungo Entkoff");
-        pc.writeFile("Lungo Entkoff");
-        play("Coffee");
- 
-    }
-    public void buttonActive2(java.awt.event.ActionEvent event){
-        System.out.println("Cappuccino");
-        pc.writeFile("Cappuccino");
-        play("Coffee");
-
-    }
-    public void buttonActive3(java.awt.event.ActionEvent event){
-        System.out.println("Cappuccino Vanilla");
-        pc.writeFile("Cappuccino Vanilla");
-        play("Coffee");
-
-    }
-    public void buttonActive4(java.awt.event.ActionEvent event){
-        System.out.println("Lungo");
-        pc.writeFile("Lungo");
-        play("Coffee");
-
-    }
-    public void buttonActive5(java.awt.event.ActionEvent event){
-        System.out.println("Latte Macchiato");
-        pc.writeFile("Latte Macchiato");
-        play("Coffee");
-
-    }
-    public void buttonActive6(java.awt.event.ActionEvent event){
-        System.out.println("Kakao");
-        pc.writeFile("Kakao");
-        play("Coffee");
-    }
-    public void buttonActive7(java.awt.event.ActionEvent event){
-        System.out.println("Tee mit Milch");
-        pc.writeFile("Tee mit Milch");
-        play("Coffee");   
-    }  
 }
 
